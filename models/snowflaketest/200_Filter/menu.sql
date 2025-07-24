b0017bd0ca3119a73c8e7572339c9a21efa7563e
Koysur Miah <koysur.miah@scania.com>
2025-07-24 11:24:36 +0000

    ADD RUNDATE FIELD FOR INCREMENTAL

--- models/snowflaketest/200_Filter/menu.sql
+++ models/snowflaketest/200_Filter/menu.sql
@@ -1,6 +1,30 @@
-{{ config(materialized='table', alias='MENU2') }}
+{{
+  config(
+    materialized='table',
+    alias='MENU2'
+  )
+}}
 
-SELECT *
-FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }}
+SELECT
+    *,
+    CURRENT_TIMESTAMP AS RUN_DATE
+  FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }}
+  WHERE MENU_TYPE IN ('Ice Cream', 'BBQ')
 
-WHERE MENU_TYPE IN ('Ice Cream','BBQ')
+
+/*
+WITH source_data AS (
+  SELECT
+    *,
+    CURRENT_TIMESTAMP AS RUN_DATE
+  FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }}
+  WHERE MENU_TYPE IN ('Ice Cream', 'BBQ')
+
+  {% if is_incremental() %}
+    AND updated_at > (SELECT MAX(updated_at) FROM {{ this }})
+  {% endif %}
+)
+
+SELECT
+  *
+FROM source_data  */
