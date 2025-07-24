afde7f4a636b71f544bd83607403401482c09aae
Koysur Miah <koysur.miah@scania.com>
2025-07-24 11:00:44 +0000

    Update 200 file

--- models/snowflaketest/200_Filter/menu.sql
+++ models/snowflaketest/200_Filter/menu.sql
@@ -1,6 +1,4 @@
 {{ config(materialized='table', alias='MENU2') }}
 
-CREATE OR REPLACE TABLE MENU AS (
-
 SELECT *
-FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }})
+FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }}
