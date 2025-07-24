56b13b10e54dabf03b953bdda09f570c8372a535
koysur.miah@scania.com <koysur.miah@scania.com>
2025-07-24 00:39:03 +0000

    This now creates a new table at every RUN

--- models/snowflaketest/200_Filter/menu.sql
+++ models/snowflaketest/200_Filter/menu.sql
@@ -1,4 +1,6 @@
 {{ config(materialized='table', alias='MENU2') }}
 
+CREATE OR REPLACE TABLE MENU AS (
+
 SELECT *
-FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }}
+FROM {{ source('KOYSURMIAH_LOAD_SAMPLE_DATA_FROM_S3', 'MENU') }})
